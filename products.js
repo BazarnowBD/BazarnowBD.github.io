2841486bf6f010009aeac27ada6839f943b5bdb6
BazarnowBD <redoyh092@gmail.com>
2025-06-30 10:55:31 +0600

    Add files via upload

---
+++ products.js
@@ -0,0 +1,43 @@
+
+const products = [
+  {
+    name: "পাঞ্জাবি",
+    price: "৳৯৫০",
+    image: "https://via.placeholder.com/150"
+  },
+  {
+    name: "সালোয়ার কামিজ",
+    price: "৳১২০০",
+    image: "https://via.placeholder.com/150"
+  },
+  {
+    name: "Samsung Galaxy A14",
+    price: "৳১৯,৯৯০",
+    image: "https://via.placeholder.com/150"
+  },
+  {
+    name: "LED লাইট সেট",
+    price: "৳৪৫০",
+    image: "https://via.placeholder.com/150"
+  },
+  {
+    name: "মিনিকেট চাল (৫ কেজি)",
+    price: "৳৪৮০",
+    image: "https://via.placeholder.com/150"
+  },
+  {
+    name: "Bata Sneakers",
+    price: "৳১৫০০",
+    image: "https://via.placeholder.com/150"
+  },
+  {
+    name: "Fair & Lovely Face Cream",
+    price: "৳১৮০",
+    image: "https://via.placeholder.com/150"
+  },
+  {
+    name: "স্কুল ব্যাগ",
+    price: "৳৫৫০",
+    image: "https://via.placeholder.com/150"
+  }
+];
